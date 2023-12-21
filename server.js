a14b9fb9425059001bec5c8fd2c4cc031d9e6149
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-12-21 11:34:29 +0100

    add_config

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 import app from "./app.js";
+import moongoose from "moongoose";
+import { DB_HOST } from "./config.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+moongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
